2fa31808af7ed44acfcf22253b75fadcbe171ce7
YorkeF <113220697+YorkeF@users.noreply.github.com>
2022-11-04 14:57:49 -0500

    Friday Programming CS2133

--- src/nov_4_2022.java
+++ src/nov_4_2022.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Main {
+public class nov_4_2022 {
     public static void main(String[] args) {
         myCanvas();
     }
