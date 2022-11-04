1e8717bbe3dcd463018a70cd4840d063ff7d15b7
YorkeF <113220697+YorkeF@users.noreply.github.com>
2022-11-04 14:54:55 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+        myCanvas();
+    }
+    public static void myCanvas() {
+        JFrame window = new JFrame();
+        JPanel panel = new JPanel();
+        window.add(panel);
+        panel.setLayout(new FlowLayout());
+
+        JButton red = new JButton("Red");
+        JButton green = new JButton("Green");
+        JButton yellow = new JButton("Yellow");
+        JButton blue = new JButton("Blue");
+
+        ActionListener colors = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource().equals(red)){ panel.setBackground(Color.RED); window.repaint(); }
+                if(e.getSource().equals(green)){ panel.setBackground(Color.GREEN); window.repaint(); }
+                if(e.getSource().equals(yellow)){ panel.setBackground(Color.YELLOW); window.repaint(); }
+                if(e.getSource().equals(blue)){ panel.setBackground(Color.BLUE); window.repaint(); }
+            }
+        };
+
+        panel.add(red); panel.add(green); panel.add(yellow); panel.add(blue);
+
+        red.addActionListener(colors); green.addActionListener(colors);
+        yellow.addActionListener(colors); blue.addActionListener(colors);
+
+        window.pack(); window.setVisible(true);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
